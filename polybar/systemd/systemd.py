d3425d6e3f68908714818c716a98017f66694bf2
Yu Qing Liu <yu.qing.liu@mail.mcgill.ca>
2024-07-01 11:15:36 -0400

    updates

--- polybar/systemd/systemd.py
+++ polybar/systemd/systemd.py
@@ -1,34 +1,43 @@
 import tkinter as tk
+import tkinter.font
+import subprocess
 
 def close_window():
     root.destroy()
 
-# Create a Tkinter window
-root = tk.Tk()
+def list_user_services():
+    try:
+        # Run systemctl command to list user services
+        output = subprocess.check_output(['systemctl', '--user', 'list-units', '--type=service', '--all']).decode('utf-8')
+        text_output.delete('1.0', tk.END)
+        text_output.insert(tk.END, output)
+    except subprocess.CalledProcessError as e:
+        text_output.delete('1.0', tk.END)
+        text_output.insert(tk.END, f"Error: {e.output.decode('utf-8')}")
 
-# Hide the title bar (make the window titleless)
-root.overrideredirect(True)
+    root.after(1000, list_user_services)
+
+# Create a Tkinter window
+root = tk.Tk(className="systemdList")
+root.title("systemd")
+root.wait_visibility(root)
+root.attributes('-alpha', 0.65)
 
 # Set window size
-window_width = 400
-window_height = 400
-
-# Calculate the position to center the window on the screen
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width - window_width) // 2
-y = (screen_height - window_height) // 2
-
-# Set window geometry to center the window
-root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+window_width = 800
+window_height = 300
 
 # Configure window background color
-root.configure(bg="white")
+root.configure(bg="black")
 
-# Create a button widget
-close_button = tk.Button(root, text="Close", command=close_window)
-close_button.pack(pady=10)  # Adjust the padding as needed
+# Create a Text widget to display output
+text_output = tk.Text(root, wrap=tk.WORD, bg="black", fg="white")
+text_output.pack(fill=tk.BOTH, expand=False, padx=20, pady=10)
+
+Desired_font = tkinter.font.Font(family = "Mono", size = 8, weight = "bold")
+text_output.configure(font = Desired_font)
+
+list_user_services()
 
 # Run the Tkinter main loop
 root.mainloop()
-
