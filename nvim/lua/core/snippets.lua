791725f2298447f0157743f62aa7e76c95921665
Yu Qing Liu <yu.qing.liu@mail.mcgill.ca>
2024-10-01 17:26:01 -0400

    updates

---
+++ nvim/lua/core/snippets.lua
@@ -0,0 +1,15 @@
+-- snippets.lua
+local ls = require("luasnip")
+
+ls.add_snippets("tex", {
+  ls.parser.parse_snippet("node", [[
+%--------------------------
+\begin{tikzpicture}
+\node [mybox] (box){%
+\begin{minipage}{0.3\textwidth}
+\end{minipage}
+};
+\node[fancytitle, right=10pt] at (box.north west) {Inter-Process Communication};
+\end{tikzpicture}
+]])
+})
