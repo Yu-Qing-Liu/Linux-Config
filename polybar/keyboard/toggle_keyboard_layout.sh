ff1adca465041edaccfbf1c669853e03cd9473be
Yu Qing Liu <yu.qing.liu@mail.mcgill.ca>
2024-11-12 15:23:06 -0500

    fr

--- polybar/keyboard/toggle_keyboard_layout.sh
+++ polybar/keyboard/toggle_keyboard_layout.sh
@@ -5,7 +5,7 @@ current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
 
 # Toggle between 'us' and 'fr'
 if [ "$current_layout" = "us" ]; then
-    setxkbmap fr
+    setxkbmap ca
 else
     setxkbmap us
 fi
