d9ca7ba5a4cf71bfe8e683618b454442f8b3f133
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-11-30 12:46:52 -0500

    updates

---
+++ i3/monitors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TARGET_RESOLUTION1="355" # vertical monitor
+TARGET_RESOLUTION2="698" # big monitor
+TARGET_RESOLUTION3="344" # laptop screen
+TARGET_MONITOR1=""
+TARGET_MONITOR2=""
+TARGET_MONITOR3=""
+
+while read -r line; do
+    if [[ $line =~ $TARGET_RESOLUTION1 ]]; then
+        TARGET_MONITOR1=$(echo $line | awk '{print $NF}')
+    fi
+    if [[ $line =~ $TARGET_RESOLUTION2 ]]; then
+        TARGET_MONITOR2=$(echo $line | awk '{print $NF}')
+    fi
+    if [[ $line =~ $TARGET_RESOLUTION3 ]]; then
+        TARGET_MONITOR3=$(echo $line | awk '{print $NF}')
+    fi
+done <<< "$(xrandr --listmonitors)"
+
+if [ -n "$TARGET_MONITOR1" ]; then
+    xrandr --output "$TARGET_MONITOR1" --rotate "right" --left-of "$TARGET_MONITOR2"
+fi
+
+if [ -n "$TARGET_MONITOR2" ]; then
+    xrandr --output "$TARGET_MONITOR2" --left-of "$TARGET_MONITOR3"
+fi
+
+if [ -n "$TARGET_MONITOR3" ]; then
+    xrandr --output "$TARGET_MONITOR3" --primary
+fi
+
+feh --bg-fill /home/admin/Pictures/Background.jpg
