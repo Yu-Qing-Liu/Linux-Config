da6da43804a85295068916125052a914e15a0cee
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-11-30 02:34:39 -0500

    updates

--- polybar/database/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if pgrep -x "pgadmin4" > /dev/null
-then
-    echo "%{B#3FFFFFFF}%{u#39949C}%{+u}  %{T3}%{T1}  %{-u}%{B-} "
-else
-    echo "%{B#0FFFFFFF}%{u#373B41}%{+u}  %{T3}%{T1}  %{-u}%{B-} "
-fi
