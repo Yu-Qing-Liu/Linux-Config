609c11ce87486c7e40a0734c3359a65954221403
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2024-12-04 10:15:54 -0500

    updates

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,37 @@
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
+# Target resolutions for identifying monitors
+TARGET_RESOLUTION1="355" # Vertical monitor
+TARGET_RESOLUTION2="698" # Big monitor
+TARGET_RESOLUTION3="344" # Laptop screen
+
+# Variables to store detected monitor names
+TARGET_MONITOR1=""
+TARGET_MONITOR2=""
+TARGET_MONITOR3=""
+
+# Parse xrandr output to find monitors matching target resolutions
+while read -r line; do
+    if [[ $line =~ $TARGET_RESOLUTION1 ]]; then
+        TARGET_MONITOR1=$(echo "$line" | awk '{print $NF}')
+    fi
+    if [[ $line =~ $TARGET_RESOLUTION2 ]]; then
+        TARGET_MONITOR2=$(echo "$line" | awk '{print $NF}')
+    fi
+    if [[ $line =~ $TARGET_RESOLUTION3 ]]; then
+        TARGET_MONITOR3=$(echo "$line" | awk '{print $NF}')
+    fi
+done <<< "$(xrandr --listmonitors)"
+
+# Assign default values if monitors are not detected
+TARGET_MONITOR1=${TARGET_MONITOR1:-"DP-2"}
+TARGET_MONITOR2=${TARGET_MONITOR2:-"eDP-1"}
+TARGET_MONITOR3=${TARGET_MONITOR3:-"eDP-1"}
+
 # Launch Polybar, using default config location ~/.config/polybar/config
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [[ $m != "DP-2" ]]; then
+    if [[ $m != $TARGET_MONITOR1 ]]; then
         MONITOR=$m polybar --reload top1 &
         MONITOR=$m polybar --reload top2 &
         MONITOR=$m polybar --reload top3 &
